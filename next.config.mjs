f31fcf69e736ff87c62801f64f5af77eebb97bfc
Emma Böcker <stckoverflw@gmail.com>
2022-10-26 16:25:21 +0200

    add pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,28 @@
 // @ts-check
 
+// @ts-ignore
+import next_pwa from "next-pwa";
+
+const withPWA = next_pwa({
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development'
+})
+
 /**
- * Don't be scared of the generics here.
- * All they do is to give us autocompletion when using this.
- *
  * @template {import('next').NextConfig} T
  * @param {T} config - A generic parameter that flows through to the return type
  * @constraint {{import('next').NextConfig}}
  */
 function defineNextConfig(config) {
-  return config;
+    return withPWA(config);
 }
 
 export default defineNextConfig({
-  reactStrictMode: true,
-  swcMinify: true,
-  // Next.js i18n docs: https://nextjs.org/docs/advanced-features/i18n-routing
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
+    reactStrictMode: true,
+    swcMinify: true,
+    // Next.js i18n docs: https://nextjs.org/docs/advanced-features/i18n-routing
+    i18n: {
+        locales: ["en"],
+        defaultLocale: "en",
+    },
 });
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,18 @@ export default function Document() {
   return (
     <>
       <Html>
-        <Head />
+        <Head>
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="Railboard" />
+          <meta
+            property="og:description"
+            content="Railboard is a simple web-app to view mainly German Stationboards"
+          />
+          <meta property="og:site_name" content="Railboard" />
+          <meta property="og:url" content="https://rail.stckoverflw.net" />
+
+          <link rel="manifest" href="/manifest.json" />
+        </Head>
         <body className={"bg-zinc-900"}>
           <Main />
           <NextScript />
