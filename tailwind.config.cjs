fdf057ff1a677625d93247662be5e02697467d97
Emma Böcker <stckoverflw@gmail.com>
2022-11-24 16:10:17 +0100

    add .idea and .vscode to prettierignore

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,17 +1,10 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
     fontFamily: {
-      sans: [
-        "Ubuntu",
-        "Roboto",
-        "Open Sans",
-        "ui-sans-serif",
-        "system-ui",
-        "sans-serif",
-      ],
+      sans: ["Ubuntu", "Roboto", "Open Sans", "ui-sans-serif", "system-ui", "sans-serif"],
     },
   },
   plugins: [],
