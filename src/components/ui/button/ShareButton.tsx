df8fe8048488b9fe8d23dbb3d2c3af29c7f9a3bd
Emma Böcker <stckoverflw@gmail.com>
2022-11-24 20:40:12 +0100

    only show share button if the api is available on client

--- src/components/ui/button/ShareButton.tsx
+++ src/components/ui/button/ShareButton.tsx
@@ -1,17 +1,20 @@
 "use client";
 
 import { ImShare2 } from "react-icons/im";
-import React, { ButtonHTMLAttributes, DetailedHTMLProps } from "react";
+import React, { ButtonHTMLAttributes, DetailedHTMLProps, useEffect, useState } from "react";
 import Button from "../../../components/ui/button/Button";
 
 export default function ShareButton(
-  props: DetailedHTMLProps<
-    ButtonHTMLAttributes<HTMLButtonElement>,
-    HTMLButtonElement
-  >
+  props: DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>
 ) {
+  const [hidden, setHidden] = useState(true);
+
+  useEffect(() => {
+    setHidden(typeof navigator !== "undefined" && navigator.share == null);
+  }, []);
+
   return (
-    <Button {...props}>
+    <Button {...props} hidden={hidden}>
       <ImShare2 size={20} />
     </Button>
   );
