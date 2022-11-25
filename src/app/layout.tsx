e0f969910091e748c506fc63f16ceae7f5230116
Emma Böcker <stckoverflw@gmail.com>
2022-11-25 16:28:27 +0100

    add more icons to fully support PWA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,11 +5,7 @@ import "../styles/globals.css";
 import "@fontsource/ubuntu";
 import "@fontsource/source-sans-pro";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <html lang="en">
@@ -17,6 +13,19 @@ export default function RootLayout({
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="manifest" href="/manifest.json" />
           <meta name="theme-color" content="#6d28d9" />
+          <link rel="icon" type="image/png" href="/assets/railboard_favicon_gray.png" />
+
+          <meta name="application-name" content="Railboard" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+          <meta name="apple-mobile-web-app-title" content="Railboard" />
+          <meta
+            name="description"
+            content="Railboard is a web-app to easily view information like delay, platform(-changes) of your Trains"
+          />
+          <meta name="format-detection" content="telephone=yes" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta name="msapplication-TileColor" content="#6d28d9" />
         </head>
         <body className={"bg-zinc-900"}>{children}</body>
       </html>
